59a262113a8741639be15155f043ba9083c54c27
Arvind2698 <r.arvind007@gmail.com>
2020-06-17 22:33:30 +0530

    UC8: Using class method for wage computation

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,63 +1,35 @@
 public class EmployeeWageComputation{
-    private int WagePerHour;
-    private int HoursPerDay;
-    private int PartTimeHours;
 
-    private int WagePerDay;
-    private int TotalWageForMonth;
-    private int TotalWorkingHours;
-    private int FullWorkingDays;
-    private int PartTimeWorkingDays;
-    private int AbsentDays;
-
-    public EmployeeWageComputation(){
-        WagePerHour=20;
-        HoursPerDay=8;
-        PartTimeHours=4;
-
-        WagePerDay=0;
-        TotalWageForMonth=0;
-        TotalWorkingHours=0;
-        FullWorkingDays=0;
-        PartTimeWorkingDays=0;
-        AbsentDays=0;
-    }
-
-    public int CalculateAttendance(){
-        return (int)(Math.random()*3);
-    }
-
-    public void CalculateWagePerDay(){
-        int EmployeeAttendance=CalculateAttendance();
-        switch(EmployeeAttendance){
-            case 0:this.WagePerDay=0;
-                this.TotalWorkingHours+=0;
-                this.AbsentDays++;
-                break;
-            case 1:this.WagePerDay=this.WagePerHour*this.HoursPerDay;
-                this.TotalWorkingHours+=this.HoursPerDay;
-                this.FullWorkingDays++;
-                break;
-            case 2:this.WagePerDay=this.WagePerHour*this.PartTimeHours;
-                this.TotalWorkingHours+=this.PartTimeHours;
-                this.PartTimeWorkingDays++;
-                break;
-            default:
-                System.out.println("Error Occurred");
-        }
-    }
-
-    public void CalculateWagePerMonth(){
+    public static void CalculateWagePerMonth(int WagePerHour,int HoursPerDay,int PartTimeHours){
+        int WagePerDay=0;
+        int TotalWageForMonth=0;
+        int TotalWorkingHours=0;
+        int FullWorkingDays=0;
+        int PartTimeWorkingDays=0;
+        int AbsentDays=0;
         for(int day=1;day<=20;day++){
-            while(this.TotalWorkingHours<100){
-                CalculateWagePerDay();
-                this.TotalWageForMonth+=this.WagePerDay;
+            while(TotalWorkingHours<100){
+                int EmployeeAttendance=(int)(Math.random()*3);
+                switch(EmployeeAttendance){
+                    case 0:WagePerDay=0;
+                        TotalWorkingHours+=0;
+                        AbsentDays++;
+                        break;
+                    case 1:WagePerDay=WagePerHour*HoursPerDay;
+                        TotalWorkingHours+=HoursPerDay;
+                        FullWorkingDays++;
+                        break;
+                    case 2:WagePerDay=WagePerHour*PartTimeHours;
+                        TotalWorkingHours+=PartTimeHours;
+                        PartTimeWorkingDays++;
+                        break;
+                    default:
+                        System.out.println("Error Occurred");
+                }
+                TotalWageForMonth+=WagePerDay;
                 break;
             }
         }
-    }
-
-    public void display(){
         System.out.println("#######################################################################");
         System.out.println("Total Wage Earned at the end of 20 days/100 hours: "+TotalWageForMonth);
         System.out.println("Total Full Working Days: "+FullWorkingDays);
@@ -66,11 +38,10 @@ public class EmployeeWageComputation{
         System.out.println("Total Hours Worked: "+TotalWorkingHours);
         System.out.println("#######################################################################");
     }
+
     public static void main(String arg[]){
         System.out.println("Welcome to the Employee Wage Calculation Program");
-        EmployeeWageComputation emp =new EmployeeWageComputation();
-        emp.CalculateWagePerMonth();
-        emp.display();
-
+        CalculateWagePerMonth(20, 8, 4);
+        CalculateWagePerMonth(40, 6, 3);
     }
 }
