e5526795441e37df108b99c74b71c7af45225819
Arvind2698 <r.arvind007@gmail.com>
2020-06-17 21:57:28 +0530

    UC5: Employee Wage for the month

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -5,20 +5,27 @@ public class EmployeeWageComputation{
     public static void main(String arg[]){
         System.out.println("Welcome to the Employee Wage Calculation Program");
         int WagePerDay=0;
+        int TotalWageForMonth=0;
 
-        int EmployeeAttendance=(int)(Math.random()*3);
-        switch(EmployeeAttendance){
-            case 0:WagePerDay=WagePerHour*0;
-            System.out.println("Employee is absent. Wage for the day: "+WagePerDay);
-            break;
-            case 1:WagePerDay=WagePerHour*HoursPerDay;
-            System.out.println("Employee is present. Wage for the day: "+WagePerDay);
-            break;
-            case 2:WagePerDay=WagePerHour*PartTimeHours;
-            System.out.println("Employee is working part-time. Wage for the day: "+WagePerDay);
-            break;
-            default:
-                System.out.println("Error Occurred");
+        for(int i=0;i<20;i++){
+            int EmployeeAttendance=(int)(Math.random()*3);
+            switch(EmployeeAttendance){
+                case 0:WagePerDay=WagePerHour*0;
+                System.out.println("Employee is absent. Wage for the day: "+WagePerDay);
+                break;
+                case 1:WagePerDay=WagePerHour*HoursPerDay;
+                System.out.println("Employee is present. Wage for the day: "+WagePerDay);
+                break;
+                case 2:WagePerDay=WagePerHour*PartTimeHours;
+                System.out.println("Employee is working part-time. Wage for the day: "+WagePerDay);
+                break;
+                default:
+                    System.out.println("Error Occurred");
+            }
+            TotalWageForMonth+=WagePerDay;
         }
+
+        System.out.println("Monthly Wage of the Employee: "+TotalWageForMonth);
+
     }
 }
