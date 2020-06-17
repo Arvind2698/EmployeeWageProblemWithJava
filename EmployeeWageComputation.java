72fce908b8051f3cb42902a17c854171ad96f156
Arvind2698 <r.arvind007@gmail.com>
2020-06-17 21:42:20 +0530

    UC2: Wage per day

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,11 +1,17 @@
 public class EmployeeWageComputation{
+    public static final int WagePerHour=20;
+    public static final int HoursPerDay=8;
     public static void main(String arg[]){
         System.out.println("Welcome to the Employee Wage Calculation Program");
+        int WagePerDay=0;
+
         int EmployeeAttendance=(int)(Math.random()*2);
         if(EmployeeAttendance==0){
-            System.out.println("Employee is absent");
+            WagePerDay=WagePerHour*0;
+            System.out.println("Employee is absent. Wage for the day: "+WagePerDay);
         }else{
-            System.out.println("Employee is present");
+            WagePerDay=WagePerHour*HoursPerDay;
+            System.out.println("Employee is present. Wage for the day: "+WagePerDay);
         }
     }
 }
