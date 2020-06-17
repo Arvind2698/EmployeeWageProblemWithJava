b0ae5af852af718be4b0f662dce584253ebb0223
Arvind2698 <r.arvind007@gmail.com>
2020-06-17 21:46:19 +0530

    UC3: Part-time employee

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,17 +1,21 @@
 public class EmployeeWageComputation{
     public static final int WagePerHour=20;
     public static final int HoursPerDay=8;
+    public static final int PartTimeHours=4;
     public static void main(String arg[]){
         System.out.println("Welcome to the Employee Wage Calculation Program");
         int WagePerDay=0;
 
-        int EmployeeAttendance=(int)(Math.random()*2);
+        int EmployeeAttendance=(int)(Math.random()*3);
         if(EmployeeAttendance==0){
             WagePerDay=WagePerHour*0;
             System.out.println("Employee is absent. Wage for the day: "+WagePerDay);
-        }else{
+        }else if(EmployeeAttendance==1){
             WagePerDay=WagePerHour*HoursPerDay;
             System.out.println("Employee is present. Wage for the day: "+WagePerDay);
+        }else{
+            WagePerDay=WagePerHour*PartTimeHours;
+            System.out.println("Employee is working part-time. Wage for the day: "+WagePerDay);
         }
     }
 }
