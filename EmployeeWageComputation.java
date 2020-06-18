ff6fd16d3b45662d416807d12aef3fe6d944473e
Arvind2698 <r.arvind007@gmail.com>
2020-06-18 08:42:26 +0530

    abstract class

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,25 +1,24 @@
 import java.util.HashMap;
-import java.util.Scanner;
 
-class Employee{
+abstract class EmployeeTemplate{
 
-    private int WagePerHour;
-    private int HoursPerDay;
-    private int PartTimeHours;
-    private String companyName;
+    public int WagePerHour;
+    public int HoursPerDay;
+    public int PartTimeHours;
 
-    private int WagePerDay;
-    private int TotalWageForMonth;
-    private int TotalWorkingHours;
-    private int FullWorkingDays;
-    private int PartTimeWorkingDays;
-    private int AbsentDays;
+    public int WagePerDay;
+    public int TotalWageForMonth;
+    public int TotalWorkingHours;
+    public int FullWorkingDays;
+    public int PartTimeWorkingDays;
+    public int AbsentDays;
 
-    public Employee(int WagePerHour,int HoursPerDay,int PartTimeHours){
+    public static HashMap<String,Integer> companies=new HashMap<>();
+
+    public EmployeeTemplate(int WagePerHour,int HoursPerDay,int PartTimeHours){
         this.WagePerHour=WagePerHour;
         this.HoursPerDay=HoursPerDay;
         this.PartTimeHours=PartTimeHours;
-        // this.companyName=companyName;
         WagePerDay=0;
         TotalWageForMonth=0;
         TotalWorkingHours=0;
@@ -32,6 +31,27 @@ class Employee{
         return (int)(Math.random()*3);
     }
 
+    public abstract void CalculateWagePerDay();
+
+    public abstract void CalculateWagePerMonth();
+
+
+    public void display(){
+        System.out.println("#######################################################################");
+        System.out.println("Total Wage Earned at the end of 20 days/100 hours: "+TotalWageForMonth);
+        System.out.println("Total Full Working Days: "+FullWorkingDays);
+        System.out.println("Total Part Time Working Days: "+PartTimeWorkingDays);
+        System.out.println("Total Absent Days: "+AbsentDays);
+        System.out.println("Total Hours Worked: "+TotalWorkingHours);
+        System.out.println("#######################################################################");
+    }
+}
+
+class Company1 extends EmployeeTemplate{
+
+    Company1(int WagePerHour,int HoursPerDay,int PartTimeHours){
+        super(WagePerHour,HoursPerDay,PartTimeHours);
+    }
     public void CalculateWagePerDay(){
         int EmployeeAttendance=CalculateAttendance();
         switch(EmployeeAttendance){
@@ -61,54 +81,48 @@ class Employee{
             }
         }
     }
+}
 
-    public int getWagePerMonth(){
-        return TotalWageForMonth;
+class Company2 extends EmployeeTemplate{
+
+    Company2(int WagePerHour,int HoursPerDay,int PartTimeHours){
+        super(WagePerHour,HoursPerDay,PartTimeHours);
+    }
+    public void CalculateWagePerDay(){
+        int EmployeeAttendance=CalculateAttendance();
+        switch(EmployeeAttendance){
+            case 0:this.WagePerDay=0;
+                this.TotalWorkingHours+=0;
+                this.AbsentDays++;
+                break;
+            case 1:this.WagePerDay=this.WagePerHour*this.HoursPerDay;
+                this.TotalWorkingHours+=this.HoursPerDay;
+                this.FullWorkingDays++;
+                break;
+            case 2:this.WagePerDay=this.WagePerHour*this.PartTimeHours;
+                this.TotalWorkingHours+=this.PartTimeHours;
+                this.PartTimeWorkingDays++;
+                break;
+            default:
+                System.out.println("Error Occurred");
+        }
     }
 
-    public void display(){
-        System.out.println("#######################################################################");
-        System.out.println("Total Wage Earned at the end of 20 days/100 hours: "+TotalWageForMonth);
-        System.out.println("Total Full Working Days: "+FullWorkingDays);
-        System.out.println("Total Part Time Working Days: "+PartTimeWorkingDays);
-        System.out.println("Total Absent Days: "+AbsentDays);
-        System.out.println("Total Hours Worked: "+TotalWorkingHours);
-        System.out.println("#######################################################################");
+    public void CalculateWagePerMonth(){
+        for(int day=1;day<=20;day++){
+            while(this.TotalWorkingHours<100){
+                CalculateWagePerDay();
+                this.TotalWageForMonth+=this.WagePerDay;
+                break;
+            }
+        }
     }
 }
 
 public class EmployeeWageComputation{
     public static void main(String arg[]){
-
-        HashMap<String,Integer> companies=new HashMap<>();
-
-        System.out.println("Enter the number of companies");
-        Scanner scanner=new Scanner(System.in);
-        int numberOfCompanies=scanner.nextInt();
-        for(int i=0;i<numberOfCompanies;i++){
-            System.out.println("Enter the wage per hour");
-            int WagePerHour=scanner.nextInt();
-            System.out.println("Enter the full-time hours");
-            int HoursPerDay=scanner.nextInt();
-            System.out.println("Enter the part-time hours");
-            int PartTimeHours=scanner.nextInt();
-            System.out.println("Enter the name of the company");
-            String CompanyName=scanner.next();
-            Employee comp=new Employee(WagePerHour,HoursPerDay, PartTimeHours);
-            comp.CalculateWagePerMonth();
-            companies.put(CompanyName,comp.getWagePerMonth());
-        }
-
-        System.out.println(companies);
-
-        System.out.println("Enter the name of the company to retrieve monthly wage");
-        String searchCompany=scanner.next();
-
-        if(companies.containsKey(searchCompany)){
-            System.out.println("Employee Wage of: "+searchCompany+" is Rs."+companies.get(searchCompany));
-        }else{
-            System.out.println("No such company");
-        }
-
+        Company1 emp1=new Company1(20, 8,4);
+        emp1.CalculateWagePerMonth();
+        emp1.display();
     }
 }
